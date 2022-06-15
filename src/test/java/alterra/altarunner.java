1d11fce8a4b813d73cdd67354bafe62351383422
salsabillaap <salsabillaalischaputri@gmail.com>
2022-06-15 15:15:53 +0700

    second commit for bdd serenity rest starter

--- src/test/java/alterra/altarunner.java
+++ src/test/java/alterra/altarunner.java
@@ -1,4 +1,15 @@
 package alterra;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+//import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "html:cucumber-report.html", "json:cucumber-json.json"},
+        features = "classpath:features/altatask"
+)
+
 public class altarunner {
 }
