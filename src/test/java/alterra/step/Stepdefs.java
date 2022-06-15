c70f14bf0f3b3ec5998a5551de35b53dd38c6b79
salsabillaap <salsabillaalischaputri@gmail.com>
2022-06-15 15:15:53 +0700

    second commit for bdd serenity rest starter

--- src/test/java/alterra/step/Stepdefs.java
+++ src/test/java/alterra/step/Stepdefs.java
@@ -1,2 +1,57 @@
-package alterra.step;public class Stepdefs {
+package alterra.step;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Stepdefs {
+    int firstnum, secondnum;
+    String operator;
+    @When("I want to count {int} and {int}")
+    public void iWantToCountAnd(int arg0, int arg1) {
+        firstnum = arg0;
+        secondnum = arg1;
+    }
+
+    @And("I am using {string} operator")
+    public void iAmUsingOperator(String arg0) {
+        operator = arg0;
+    }
+
+    @Then("I receive {int} from this count")
+    public void iReceiveFromThisCount(int arg0) {
+        switch (operator){
+            case "+" :
+                if (firstnum + secondnum == arg0){
+                    System.out.println("Result: " + (firstnum+secondnum) + " Sesuai");
+                } else{
+                    System.out.println("Tidak sesuai");
+                }
+                break;
+            case "-" :
+                if (firstnum - secondnum == arg0){
+                    System.out.println("Result: " + (firstnum-secondnum) + " Sesuai");
+                } else{
+                    System.out.println("Tidak sesuai");
+                }
+                break;
+            case "*" :
+                if (firstnum * secondnum == arg0){
+                    System.out.println("Result: " + (firstnum*secondnum) + " Sesuai");
+                } else{
+                    System.out.println("Tidak sesuai");
+                }
+                break;
+            case "/" :
+                if (firstnum / secondnum == arg0){
+                    System.out.println("Result: " + (firstnum/secondnum) + " Sesuai");
+                } else{
+                    System.out.println("Tidak sesuai");
+                }
+                break;
+            default:
+                System.out.println("Tidak sesuai");
+                break;
+        }
+    }
 }
